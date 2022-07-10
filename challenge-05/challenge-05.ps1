4b049809499531eba13fc9c294a3444c05ae15ea
unknown <andrei.agape@banshie.com>
2022-07-10 19:45:46 +0200

    challenge 5

---
+++ challenge-05/challenge-05.ps1
@@ -0,0 +1,14 @@
+﻿$resourceGroupName = "AZ-CTF"
+$vmName = "0xpwnvm"
+$vmUser = "0xpwn"
+$vmPass = "0xPwn0xPwn!!!"
+$location = "West Europe"
+
+az login
+
+az group delete --name $resourceGroupName --yes
+az group create --name $resourceGroupName --location $location
+
+az vm create --resource-group $resourceGroupName --name $vmName --image "UbuntuLTS" --admin-username $vmUser --admin-password $vmPass --authentication-type "password" --location $location --custom-data cloud-init.txt
+
+az vm open-port --port 80 --resource-group $resourceGroupName --name $vmName
